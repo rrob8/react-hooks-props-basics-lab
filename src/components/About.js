4adab5bc5b711366bc25458aaf2abbd400d26596
Robert Bubnik <rbubnik@gmail.com>
2023-10-01 19:47:28 -0400

    passing all tests

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-       <p>{props.bio && props.bio !== '' ? props.bio : null }
-       </p>
+      {props.bio ? <p>{props.bio}</p> : null }
+      
             
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links 
